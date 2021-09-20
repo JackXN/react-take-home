3ec3f702d571787f725edbe363eec7764a12c03e
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-20 09:51:50 -0600

    finnish

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,80 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import Header from './components/Header';
+import axios from 'axios'
+import {Header, Media, Buttons} from './components/Styles';
 
 
 
 function App() {
 
-const [appInfo, setAppInfo] = useState();
 
-  useEffect(() => {
-    fetch('https://www.plugco.in/public/take_home_sample_feed')
-    .then((res) => res.json())
-    .then(res => console.log(res))
-    .then((res) => res.campaigns.map((camp) => {
-      return {
-        name: camp.campaign_name
-      }
-    } ))
-  }, [])
+  //Initial data value is null
+  const [data, setData] = useState(null);
 
+const getData = () => {
+  const URL = 'https://www.plugco.in/public/take_home_sample_feed';
+  axios.get(URL).then(res => {
+    // after fetching the data set the value to the fetched data
+    setData(res.data.campaigns);
+  })
+  // If there is an error console.log the error and set the data values to the error
+  .catch((error) => {
+    console.log(error);
+    setData(error);
+  })
+}
+
+
+// Run the getData function on mount
+useEffect(() => {
+  getData()
+}, [])
+
+
+const copy = (link) => {
+  navigator.clipboard.writeText(link)
+}
+
+
+
+  console.log(data)
 
   return (
     <div className="App">
-        <h1>PLUGS</h1>
-          <hr/>
-      <div className='header_container'>
-        <Header/>
-      </div>
+      {/* if data === null display loading */}
+      {data === null && <h1>Loading...</h1>}
+      {/* If there is an error display failed request */}
+      {data === 'error' && <h1>Failed Request! Please try again later</h1>}
+      {/* If data is fetched succesfully, map through */}
+      {data !== null && data.map((cam, index) => {
+        return (
+          <div key={index}>
+            <Header>
+          <img src={cam['campaign_icon_url']} alt='icon'/>
+          <div>
+            <h3>{cam['campaign_name']}</h3>
+            <p>{cam['pay_per_install']}</p>
+          </div>
+            </Header>
+            <Media>
+              {
+                cam.medias.map((content, index) => {
+                  return (
+                    <div className='media' key={index}> 
+                    <img src={content['cover_photo_url']} alt=''/>
+                    {content['media_type'] === 'video' &&<i id='play' className='fas fa-play'></i>}
+                    <Buttons>
+                    <button onClick={() => copy(content['tracking_link'])}><i className="fal fa-link fa-flip-vertical"></i> </button>
+                        <button><a href={content['download_url']} download><i className="fal fa-arrow-to-bottom"></i></a></button>
+                    </Buttons>
+                    </div>
+                  )
+                })
+              }
+            </Media>
+            </div>
+        )
+      })}
     </div>
   );
 }
---
+++ app/src/components/Styles.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`{
+    display: flex;
+    align-items: center;
+    margin: 15px;
+    img {
+        width: 50px;
+        height: 50px;
+        border-radius: 30%;
+    };
+    div {
+        display: flex;
+        flex-direction: column;
+        margin: 15px;
+        p {
+            color: green;
+            margin-top: 5px;
+        };
+    };
+}`;
+
+export const Media = styled.div`{
+    background-color: #f4f7fb;
+    display: flex;
+    flex-direction: row;
+    overflow: scroll;
+    padding: 15px;
+    .media {
+        margin-right: 8px;
+        padding-right: 5px;
+        height: 235px;
+        img {
+            height: 185px;
+            width: 108px;
+            opacity: .6;
+        };
+        #play {
+            color: #27cc5c;
+            position: relative;
+            bottom: 95px;
+            left: 40px;
+            font-size: 1.5em;
+            opacity: 1;
+        };
+    };
+}`;
+
+export const Buttons = styled.div`{
+    display: flex;
+    width: 110px;
+    justify-content: space-between;
+    position: relative;
+    bottom: 20px;
+    button {
+        height: 35px;
+        width: 70px;
+        border: solid #cddee6 1px;
+        border-radius: 5px;
+        background-color: white;
+        color: gray;
+        font-size: 1em;
+        cursor: pointer;
+        a {
+            color: gray;
+            :hover {
+                color: blue;
+            };
+        };
+        :hover {
+            color: blue;
+        };
+    };
+}`;
